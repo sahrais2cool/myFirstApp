7fdf3e5e727551d8c60628e3fa368923f48de9fa
sahra <sahravann0@gmail.com>
2023-07-12 14:36:23 -0700

    Initial Commit

---
+++ myFirstApp/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  myFirstApp
+//
+//  Created by Scholar on 7/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+       
+        VStack {
+            Text("Teddy lol")
+                .font(.largeTitle)
+                .fontWeight(.thin)
+                .foregroundColor(Color.purple)
+            Image("teddy")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .padding(2.0)
+        }
+        
+        
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
